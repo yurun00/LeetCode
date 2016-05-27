c83d3e81dc8d19f024f69e3f7392e720e55cb74e
yurun <yurun00@gmail.com>
2016-05-27 17:15:33 +0800

    RegularExpressionMatching-DP solution

--- LeetCode/RegularExpressionMatching.cpp
+++ LeetCode/RegularExpressionMatching.cpp
@@ -22,12 +22,19 @@ isMatch("aab", "c*a*b") ¡ú true
 using namespace std;
 
 int main() {
-	cout << Solution::isMatch("aa", "a") << endl;
-	cout << Solution::isMatch("aa", "aa") << endl;
-	cout << Solution::isMatch("aaa", "aa") << endl;
-	cout << Solution::isMatch("aa", "a*") << endl;
-	cout << Solution::isMatch("aa", ".*") << endl;
-	cout << Solution::isMatch("ab", ".*") << endl;
-	cout << Solution::isMatch("aab", "c*a*b") << endl;
+	//cout << Solution::isMatch("aa", "a") << endl;
+	//cout << Solution::isMatch("aa", "aa") << endl;
+	//cout << Solution::isMatch("aaa", "aa") << endl;
+	//cout << Solution::isMatch("aa", "a*") << endl;
+	//cout << Solution::isMatch("aa", ".*") << endl;
+	//cout << Solution::isMatch("ab", ".*") << endl;
+	//cout << Solution::isMatch("aab", "c*a*b") << endl;
+	cout << Solution::isMatchDP("aa", "a") << endl;
+	cout << Solution::isMatchDP("aa", "aa") << endl;
+	cout << Solution::isMatchDP("aaa", "aa") << endl;
+	cout << Solution::isMatchDP("aa", "a*") << endl;
+	cout << Solution::isMatchDP("aa", ".*") << endl;
+	cout << Solution::isMatchDP("ab", ".*") << endl;
+	cout << Solution::isMatchDP("aab", "c*a*b") << endl;
 	return 0;
 }
--- LeetCode/RegularExpressionMatching.h
+++ LeetCode/RegularExpressionMatching.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
@@ -17,6 +18,41 @@ public:
 			return isMatch(s, p.substr(2)) || !s.empty() && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p);
 		else
 			return !s.empty() && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p.substr(1));
+	}
+
+	static bool isMatchDP(string s, string p) {
+		/**
+		* m[i][j]: if s[0,...,i-1] matches p[0,...,j-1];
+		* if p[j-1] != '*'
+		*	m[i][j] = (s[i-1] == p[j-1] || p[j-1] == '.') && m[i-1][j-1];
+		* else, p[j-1] == '*'
+		*	m[i][j] = m[i][j-2], if p[j-2] repeats 0 times
+		*	|| (s[i-1] == p[j-2] || p[j-2] == '.') && m[i-1][j], if p[j-2] repeats > 0 times
+		**/
+		if (p[0] == '*')
+			return false;
+
+		int len1 = s.length(), len2 = p.length();
+		vector<vector<bool>> m(len1 + 1, vector<bool>(len2 + 1, false));
+
+		m[0][0] = true;
+
+		m[0][1] = false;
+		for (int i = 2;i <= len2;i++)
+			m[0][i] = m[0][i - 2] && p[i - 1] == '*';
+
+		for (int i = 1;i <= len1;i++)
+			m[i][0] = false;
+
+		for (int i = 1;i <= len1;i++) 
+			for (int j = 1;j <= len2;j++) {
+				if (p[j - 1] != '*')
+					m[i][j] = (s[i - 1] == p[j - 1] || p[j - 1] == '.') && m[i - 1][j - 1];
+				else
+					m[i][j] = m[i][j - 2] || (s[i - 1] == p[j - 2] || p[j - 2] == '.') && m[i - 1][j];
+			}
+
+		return m[len1][len2];
 	}
 };
 
