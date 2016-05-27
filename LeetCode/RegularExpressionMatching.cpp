956b5d180ed857b92d5009206e8db370f252309f
yurun <yurun00@gmail.com>
2016-05-27 22:01:07 +0800

    ContainerWithMostWater

--- LeetCode/RegularExpressionMatching.cpp
+++ LeetCode/RegularExpressionMatching.cpp
@@ -17,18 +17,19 @@ isMatch("ab", ".*") ¡ú true
 isMatch("aab", "c*a*b") ¡ú true
 */
 
-#include <iostream>
+/*#include <iostream>
 #include "RegularExpressionMatching.h"
 using namespace std;
 
 int main() {
-	//cout << Solution::isMatch("aa", "a") << endl;
-	//cout << Solution::isMatch("aa", "aa") << endl;
-	//cout << Solution::isMatch("aaa", "aa") << endl;
-	//cout << Solution::isMatch("aa", "a*") << endl;
-	//cout << Solution::isMatch("aa", ".*") << endl;
-	//cout << Solution::isMatch("ab", ".*") << endl;
-	//cout << Solution::isMatch("aab", "c*a*b") << endl;
+	cout << Solution::isMatch("aa", "a") << endl;
+	cout << Solution::isMatch("aa", "aa") << endl;
+	cout << Solution::isMatch("aaa", "aa") << endl;
+	cout << Solution::isMatch("aa", "a*") << endl;
+	cout << Solution::isMatch("aa", ".*") << endl;
+	cout << Solution::isMatch("ab", ".*") << endl;
+	cout << Solution::isMatch("aab", "c*a*b") << endl;
+
 	cout << Solution::isMatchDP("aa", "a") << endl;
 	cout << Solution::isMatchDP("aa", "aa") << endl;
 	cout << Solution::isMatchDP("aaa", "aa") << endl;
@@ -37,4 +38,4 @@ int main() {
 	cout << Solution::isMatchDP("ab", ".*") << endl;
 	cout << Solution::isMatchDP("aab", "c*a*b") << endl;
 	return 0;
-}
+}*/
